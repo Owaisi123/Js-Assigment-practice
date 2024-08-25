7ab69b0847e8c77bebecabc27af06b631160865b
Owaisi123 <165272983+Owaisi123@users.noreply.github.com>
2024-08-25 14:08:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+// Calculator syntax by if condition:
+
+
+// var num1 =+prompt("Enter first value?");
+// var num2 =+prompt("Enter second value?");
+// var operator = prompt("Choose operator");
+
+// if (operator == "+") {
+//     var result=num1+num2;
+//   alert(result);
+// } else if (operator == "-") {
+//     var result=num1-num2;
+//   alert(result);}
+//  else if (operator == "*"){
+//     var result=num1*num2;
+//     alert(result);
+// }else if(operator == "/"){
+//     var result=num1/num2;
+//     alert(result);
+// }
+
+
+
+
+
+
+//  Entry:
+
+// var myName=prompt("Enter Your Name? First letter should be Capital." );
+// if (myName == "Owais") {
+//     alert("You can come");
+// }else if(myName=="Asifa"){
+// alert("You can come")
+// }else{
+//     alert("You can't come")
+// }
+
+
+
+
+// Check Result:
+
+// var myGrade=prompt("Enter Your Grade")
+// if (myGrade == "A+") {
+//     alert("You can take admission in DJ COllege!")
+// }else{
+//     alert("You are not eligible for taking admission here!")
+// }
+
+
+
+
+
+
+// Check elibglity:
+
+
+// var myAge=prompt("Enter Your Age?");
+// if (myAge==18) {
+//     alert("You can get NIC!")
+// }else if (myAge == 17) {
+//     alert("You can get NIC soon!")
+// }else{
+//     alert("You can't get NIC right now!")
+// }
+
+
+
+
+
+
+
+// Check elibglity for going abroad:
+
+
+// var myPasport=prompt("Do you have pasport?")
+
+// if (myPasport == "Yes") {
+//     alert("You can go abroad!")
+// }else if(myPasport=="yes"){
+// alert("You can go abroad!")
+// }
+// else{
+//     alert("You can't go abroad without pasport!")
+// }
+
+
+
+
+// var myBalance=prompt("Enter Your Balance?")
+
+// if (myBalance == 5000) {
+//     alert("You can take money!")
+// }else if(myBalance=="Five Thousand"){
+//     alert("You can take money!")
+// }else if (myBalance=="five thousand"){
+//     alert("You can take money!")
+// }
+// else{
+//     alert("You can't take money!")
+// }
+
+
+
+
+
+
+// var myTeam=prompt("Enter favourite cricket team name? Pakistan,England,Newzealand,India,Austrailia. Check win probablity?" )
+
+// if (myTeam=="Pakistan") {
+//     alert("Win pridiction 70%")
+// }else if(myTeam=="England"){
+//     alert("Win pridiction 80%")
+// }else if(myTeam=="Newzealand"){
+//     alert("Win pridiction 85%")
+// }else if(myTeam=="India"){
+//     alert("Win pridiction 60%")
+// }else if(myTeam=="Austrailia"){
+//     alert("Win pridiction 90%")
+// }
